15c19fe1b06ab1afe04fbf20beaae26c5123d0b5
김수빈 <38367572+Ksubin0@users.noreply.github.com>
2025-02-15 18:19:17 +0900

    [프로젝트] 외부에서 Back에 접근하기 위한 설정 추가
    Nginx(외부)에서 Back에 접근하기 위해 host=0.0.0.0 추가. 이 내용이 없으면 localhost(127.0.0.1)에서만 접근 가능함.

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ app.register_blueprint(trading_bp, url_prefix='/api/trading')
 app.register_blueprint(openai_bp, url_prefix='/api/openai')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=5000)
